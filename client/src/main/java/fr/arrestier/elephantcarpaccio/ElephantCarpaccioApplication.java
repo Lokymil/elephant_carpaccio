82d1c279bcdc8b676e7deaf8a33dd6591f08b765
Christophe ARRESTIER <arrestierchristophe@gmail.com>
2022-01-11 18:36:24 +0100

    initialize java client

---
+++ client/src/main/java/fr/arrestier/elephantcarpaccio/ElephantCarpaccioApplication.java
@@ -0,0 +1,13 @@
+package fr.arrestier.elephantcarpaccio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElephantCarpaccioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElephantCarpaccioApplication.class, args);
+	}
+
+}
---
+++ client/src/test/java/fr/arrestier/elephantcarpaccio/ElephantCarpaccioApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arrestier.elephantcarpaccio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElephantCarpaccioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
