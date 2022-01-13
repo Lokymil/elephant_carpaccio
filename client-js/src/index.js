b56c434b9db3a6e06fdbf50fc3131db85904b36c
Christophe ARRESTIER <arrestierchristophe@gmail.com>
2022-01-13 11:57:30 +0100

    Make JS client listen to server

--- client-js/src/index.js
+++ client-js/src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const io = require("socket.io-client");
+
+const socket = io("http://localhost:3000/team");
+
+socket.on("connect", () => {
+  console.log(`Connected with id : ${socket.id}`);
+  socket.emit("auth", "JS");
+});
+
+socket.on("cart", (cart) => {
+  console.log(cart);
+});
+
+socket.on("disconnect", () => {
+  console.log("Disconnected"); // undefined
+});
