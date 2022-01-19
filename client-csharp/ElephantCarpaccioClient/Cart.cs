cf12563283f366974c22597c5dc89a8fd0bcf6df
Maxime MADER <maxime.mader@kawabytes.com>
2022-01-19 16:36:17 +0100

    Quick'n'dirty csharp client.

---
+++ client-csharp/ElephantCarpaccioClient/Cart.cs
@@ -0,0 +1,8 @@
+record Cart(List<int> prices, List<int> quantities, string country, string reduction)
+{
+    public string computeInvoice()
+    {
+        // TODO: return the correct invoice value!
+        return "0 €";
+    }
+}
---
+++ client-csharp/ElephantCarpaccioClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using SocketIOClient;
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+const string cartEventName = "cart";
+const string invoiceEventName = "invoice";
+
+var teamName = configuration["Settings:Team"];
+var uri = $"http://{configuration["Settings:Host"]}:3000/team";
+
+var client = new SocketIO(uri, new()
+{
+    Query = new List<KeyValuePair<string, string>>
+    {
+        new("Reconnection", "true")
+    }
+});
+
+client.On(cartEventName, response =>
+{
+    Console.WriteLine($"Cart: {response}");
+
+    var cart = response.GetValue<Cart>();
+
+    var invoice = cart.computeInvoice();
+    
+    Console.WriteLine($"Sending: {invoice}");
+
+    Task.Run(async() => await client.EmitAsync(invoiceEventName, invoice));
+});
+
+client.On(invoiceEventName, response =>
+{
+    var result = response.GetValue<string>();
+    
+    Console.WriteLine($"Result: {result}");
+});
+
+client.OnConnected += async (sender, e) =>
+{
+    Console.WriteLine($"Connected with id: {client.Id}");
+    
+    await client.EmitAsync("auth", teamName);
+};
+
+client.OnDisconnected += async (sender, e) =>
+{
+    Console.WriteLine($"Disconnected: {e}");
+};
+
+await client.ConnectAsync();
+
+Console.WriteLine("Press any key to exit ...");
+Console.Read();
