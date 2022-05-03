b118adf60170c9c39fcc906087ccc1cfcdf037d9
Christophe ARRESTIER <arrestierchristophe@gmail.com>
2022-05-03 12:01:33 +0200

    Fix conf file

--- server/src/conf.ts
+++ server/src/conf.ts
@@ -1,7 +1,7 @@
 export const cartRate = 10000; // milliseconds - default is 10000 = 10 sec
-export const totalDuration = 120000; // milliseconds - default is 2400000 = 40 min
+export const totalDuration = 2400000; // milliseconds - default is 2400000 = 40 min
 export const startDifficulty = 0; // check difficulty/difficulty.ts file - default is 0 = easiest
 export const wrongAnswerFactor = 0.5; // default is 0.5, they will loose half point they should have win with valid answer
 export const noAnswerFactor = 1; // default is 1, they will loose as many point as they should have win with valid answer
-export const validAnswerStreakThreshold = 1; // default is 10
+export const validAnswerStreakThreshold = 10; // default is 10
 export const countTeamWithHighStreakThreshold = 1; // default is 1
