6f380d632c0adf7c8a539b8101ebe131d502981b
RN <rubenabet@gmail.com>
2022-11-22 17:55:24 +0100

    feat: display team ranks in scoreboard

--- scoring/src/components/teams/Team.js
+++ scoring/src/components/teams/Team.js
@@ -3,7 +3,7 @@ import './Team.css';
 const Team = ({ team: { name, points, totalWins, winStreak, connected } }) => {
   return (
     <div className={`team ${connected ? 'connected' : 'disconnected'}`}>
-      <h2>{name}</h2>
+      <h2>{points !== 0 ? `${rank}. ` : null}{name}</h2>
       <div>Score : {points}</div>
       <div>Wins : {totalWins}</div>
       <div>Streak : {winStreak}</div>
