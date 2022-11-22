a933a0677ed83f6dc685a0ea5f6d351e7d63a2c2
RN <rubenabet@gmail.com>
2022-11-22 17:54:56 +0100

    feat: display team's total nbr of wins in scorebrd

--- scoring/src/components/teams/Team.js
+++ scoring/src/components/teams/Team.js
@@ -1,10 +1,11 @@
 import './Team.css';
 
-const Team = ({ team: { name, points, winStreak, connected } }) => {
+const Team = ({ team: { name, points, totalWins, winStreak, connected } }) => {
   return (
     <div className={`team ${connected ? 'connected' : 'disconnected'}`}>
       <h2>{name}</h2>
       <div>Score : {points}</div>
+      <div>Wins : {totalWins}</div>
       <div>Streak : {winStreak}</div>
       <div className={`connection ${connected ? 'connected' : 'disconnected'}`}>
         {connected ? 'Connected' : 'Disconnected'}
